3524f4988c5e780fb1486891a7b90b0d4cf7d915
Christina Oliveira <ChristinaOliveira@USERs-MacBook.local>
2019-01-31 13:01:19 -0800

    base implementations of client and server

--- client.cpp
+++ client.cpp
@@ -1,52 +1,169 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sstream>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
 #include <netinet/in.h>
-#include <fstream>
-#include <string>
-#include <thread>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/fcntl.h>
+#include <sys/unistd.h>
+#include <sys/time.h>
 #include <iostream>
 
 int main (int argc, char* argv[]) {
 
-  int socket_fd;
-  char *filename;
-  char *hostname;
-  int port;
+  int socket_fd, status;
+  char *filename, *hostname, *port; 
+  struct addrinfo *addr;
+  struct addrinfo hin; 
+  int one = 1;
 
-
-  if (argc != 3) {
+ //should only be 3 arguments
+ if (argc != 4) {
     fprintf(stderr, "ERROR: Incorrect number of args\n");
     exit(1);
   }
 
-  else {
-  	hostname = argv[0];
-  	port = atoi(argv[1]);
-    filename = argv[2];
-  }
-
-//TODO
-  if (port < 3) {
+  //port number should not be in 0-1023 range
+  if (atoi(port) <= 1023) {
     fprintf(stderr, "ERROR: Invalid port number\n");
     exit(1);
   }
-
-  if (hostname == "hewwo") {
-    fprintf(stderr, "ERROR: Invalid hostname\n");
-    exit(1);
+  else {   //parse arguments
+  	hostname = argv[1];
+  	port = argv[2];
+    filename = argv[3];
   }
-
-
-  if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+//addtional arg error checking
+  memset(&hin, 0, sizeof(hin));
+  hin.ai_family = AF_INET;
+  hin.ai_socktype = SOCK_STREAM; 
+  status = getaddrinfo(hostname, port, &hin, &addr);
+  if (status != 0)
+    {
+      fprintf(stderr, "ERROR: Argument Error\n");
+      exit(1);
+    }
+//open the socket
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     fprintf(stderr, "ERROR: Could not create socket\n");
     exit(1);
   }
+
+  //make socket reusable
+  if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)) == -1)
+    {
+      fprintf(stderr,"ERROR: Call to setsockopt() failed\n");
+      exit(1);
+    }
+//make socket non-blocking
+  if (fcntl(socket_fd, F_SETFL, O_NONBLOCK))
+    {
+      fprintf(stderr, "ERROR: Unable to get socket to nonblocking\n");
+      exit(1);
+    }
+//timeout stuff
+  fd_set active_fd_set;
+  struct timeval timeout;
+  FD_ZERO(&active_fd_set);
+  FD_SET(socket_fd, &active_fd_set);
+  timeout.tv_sec = 15; 
+  timeout.tv_usec = 0;
   
+
+  int rec = connect(socket_fd, addr->ai_addr, addr->ai_addrlen); 
+  if ( rec < 0)
+    {
+      if(errno == EINPROGRESS)
+	{
+
+	  int recsel = select(socket_fd+1, NULL, &active_fd_set, NULL, &timeout);
+	  if(recsel < 0)
+	    {
+	      fprintf(stderr, "ERROR: Unable to select\n");
+	      exit(1);
+	    }
+	  else if( recsel == 0)
+	    {
+	      fprintf(stderr, "ERROR: Connection timedout\n");
+	      exit(1);
+	    }
+	  else
+	    {
+	      printf("Connection success.\n");	 //need?     
+	    }
+	}
+      else
+	{
+	  fprintf(stderr,"ERROR: Unable to connect socket\n");
+	  exit(1);
+	}
+    }
+  else
+    {
+      fprintf(stderr,"ERROR: Unable to connect socket\n");
+      exit(1);
+    }
+
+  struct sockaddr_in client_addr;
+  socklen_t client_addr_len = sizeof(client_addr);
+  if (getsockname(socket_fd, (struct sockaddr *)&client_addr, &client_addr_len) == -1)
+    {
+      fprintf(stderr,"ERROR: Call to getsockname() failed\n");
+      exit(1);
+    }
+  
+  char IP_string[INET_ADDRSTRLEN] = {'\0'};
+  inet_ntop(client_addr.sin_family, &client_addr.sin_addr, IP_string, sizeof(IP_string));
+  std::cout << "Set up a connection from: " << IP_string << ":" <<
+    ntohs(client_addr.sin_port) << std::endl;
+
+  FILE* f_pointer= fopen(filename,"rb");
+  if(f_pointer == NULL){
+      fprintf(stderr, "ERROR: File could not be opened\n");
+      exit(1);
+    }
+  while(1){
+      int recsel = select(socket_fd+1, NULL, &active_fd_set, NULL, &timeout);
+      if(recsel < 0){
+	  fprintf(stderr, "ERROR: Select failed\n");
+	  exit(1);
+	}
+      else if(recsel==0){
+	  fprintf(stderr, "ERROR: Connection timeout\n");
+	  exit(1);
+	}
+	//use buffer to read
+      char buffer[1024];
+      int buffer_size = 1024;
+      memset(buffer, '\0', buffer_size);
+      int read_byte = fread(buffer,sizeof(char), buffer_size, f_pointer);
+      if(read_byte == 0){
+	  fprintf(stdout, "No bytes are read\n");
+	  fclose(f_pointer);
+	  break;
+	} 
+      else if (read_byte < 0){
+	  fprintf(stderr, "ERROR: File read error\n");
+	  exit(1);
+	}
+      else{
+	  int byte_sent = send(socket_fd, buffer, read_byte, 0);
+	  fprintf(stdout, " %d bytes sent.\n", byte_sent);
+	  if(byte_sent < 0)
+	    {
+	      fprintf(stderr, "ERROR: Unable to send files\n");
+	      exit(1);
+	    }
+	}
+    } 
   close(socket_fd);
+  freeaddrinfo(addr);
+  printf("Program Exited successfully\n");
+  exit(0);
 }
+
+
--- server.cpp
+++ server.cpp
@@ -1,39 +1,187 @@
-#include <string>
-#include <thread>
-#include <iostream>
-#include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <signal.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <iostream>
+
+//TODO: sigquit or sigterm
+
+//gloabals needed
+ void* connection_handler(void* param);
+ void signal_handler(int signo);
+ char *file_directory;
+  int connection_num = 0;
 
 int main (int argc, char* argv[]) {
-
-  int socket_fd;
-  char *file_directory;
-  int port;
-
-  if (argc != 2) {
+  int socket_fd, status;
+  char* port;
+  int one = 1;
+//should have 3 args
+  if (argc != 3) {
     fprintf(stderr, "ERROR: Incorrect number of args\n");
     exit(1);
   }
-
-  else {
-  	port = atoi(argv[0]);
-  	file_directory = argv[1];
+  else {  //parse arguments
+  	port = argv[1];
+  	file_directory = argv[2];
   }
-
-//TODO
-  if (port < 3) {
+  //port number should not be between 0 and 1023
+  if (atoi(port) <= 1023) {
     fprintf(stderr, "ERROR: Invalid port number\n");
     exit(1);
   }
+  //signal handlers
+  signal(SIGQUIT, signal_handler);
+  signal(SIGTERM, signal_handler);
 
-  if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+  struct addrinfo hin;
+  struct addrinfo *addr;  
+  memset(&hin, 0, sizeof hin); 
+  hin.ai_family = AF_INET;     
+  hin.ai_socktype = SOCK_STREAM; 
+  hin.ai_flags = AI_PASSIVE; //have IP filled
+  if ((status = getaddrinfo(NULL, port , &hin, &addr)) != 0){
+      fprintf(stderr, "ERROR: %s", gai_strerror(status));
+      exit(1);
+    }
+
+  if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     fprintf(stderr, "ERROR: Could not create socket\n");
     exit(1);
   }
-  
-  close(socket_fd);
+
+  if (setsockopt(socket_fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int)) == -1){
+      fprintf(stderr,"ERROR: call to setsockopt() failed\n");
+      exit(1);
+    }
+
+  if (bind(socket_fd, addr->ai_addr, addr-> ai_addrlen) == -1) {
+      fprintf(stderr, "ERROR: Unable to bind socket to address\n");
+    	exit(1);
+  }
+    if (listen(socket_fd, 10) == -1) {
+      fprintf(stderr, "ERROR: Socket unable to listen\n");
+    	exit(1);
+  }
+while(true){
+      struct sockaddr_in client_addr;
+      socklen_t client_addr_size = sizeof(client_addr);
+      int client_socket_fd = accept(socket_fd, (struct sockaddr*)&client_addr, &client_addr_size);
+      if (client_socket_fd == -1){
+	      fprintf(stderr, "ERROR: Socket accept failed\n");
+	      exit(1);   
+	}
+      else {
+	  connection_num++;
+	  char ipstr[INET_ADDRSTRLEN] = {'\0'};
+	  inet_ntop(client_addr.sin_family, &client_addr.sin_addr, ipstr, sizeof(ipstr));
+	  std::cout << "Accept a connection from: " << ipstr << ":" <<
+	    ntohs(client_addr.sin_port) << std::endl;
+	  
+	  pthread_t thread_id;
+	  //create thread and send to connection handler
+	  if(pthread_create(&thread_id, NULL, connection_handler, (void*)&client_socket_fd)){
+	      fprintf(stderr, "ERROR: pthread creation error\n");
+	      exit(1);
+	    }
+	  pthread_detach(thread_id);
+	}	
+    }
+  freeaddrinfo(addr);
+  exit(0);
+}
+void signal_handler(int signo)
+{
+  if (signo == SIGQUIT || signo == SIGTERM)
+    exit(0);
 }
 
+void* connection_handler(void* param)
+{
+  char f_path[1000];
+  sprintf(f_path,"%s/%d.file", file_directory, connection_num);
+  printf("Enters connection handler\n");
+  int client_socket_fd = *(int*)param;
+  if (fcntl(client_socket_fd, F_SETFL, O_NONBLOCK))
+    {
+      fprintf(stderr, "ERROR: set nonblocking error\n");
+      exit(1);
+      }
+  fd_set active_fd_set;
+  FD_ZERO(&active_fd_set);
+  FD_SET(client_socket_fd, &active_fd_set);
+  struct timeval timeout;
+  timeout.tv_sec = 15; 
+  timeout.tv_usec = 0;
+  FILE* f_pointer = fopen(f_path, "wb");
+  printf("File %s created\n", f_path);
+  if(f_pointer == NULL)
+    {
+      fprintf(stderr, "ERROR: create file failed\n");
+      exit(1);
+    }
+  while(1)
+    {
+      int recsel = select(client_socket_fd+1, &active_fd_set, NULL, NULL, &timeout);
+	if(recsel < 0)
+	  {
+	    fprintf(stderr, "ERROR: Select failed\n");
+	    exit(1);
+	  }
+	else if( recsel == 0){
+	    close(client_socket_fd);
+	    fprintf(stderr, "ERROR: Connection timeout\n");
+	    fclose(f_pointer);
+	    f_pointer = fopen(f_path, "wb");
+	    printf("Reopen\n");
+	    if(f_pointer == NULL){
+		fprintf(stderr, "ERROR: file reopen error\n");
+		exit(1);
+	      }
+	    char message[20] = "ERROR";
+	    fflush(f_pointer);
+	    int written = fwrite(message, sizeof(char), 6, f_pointer);
+	    printf("%d written\n", written);
+	    if( written < 0){
+		fprintf(stderr, "ERROR: Write error failed\n");
+		exit(1);
+	      }
+	    fclose(f_pointer);
+	    return 0;
+	  }
+
+	char buffer[1024];
+	int buffer_size = 1024;
+	memset(buffer, '\0', buffer_size);
+	int byte_received = recv(client_socket_fd, buffer, buffer_size, 0);
+	printf("%d bytes received\n", byte_received);
+	if(byte_received < 0){
+	    fprintf(stderr, "ERROR: Receiving data failed\n");
+	    exit(1);
+	  }
+	else if(byte_received == 0){ 
+	    printf("Nothing recieved\n");
+	    fclose(f_pointer);
+	    break;
+	  }
+	else{
+	    printf("Received %d bytes\n", byte_received);
+	    if(fwrite(buffer, sizeof(char), byte_received, f_pointer) ==  0){
+		fprintf(stderr, "ERROR: Write to file failed\n");
+		exit(1);
+	      }
+	  }
+	    	
+    }
+  return 0; 
+  
+}
