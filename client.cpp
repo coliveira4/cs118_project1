f1322c64b2198684f41126cff083bb97e1f0d521
Christina Oliveira <ChristinaOliveira@USERs-MacBook.local>
2019-01-31 09:52:30 -0800

    input parsing anderror checking and opening the socket for client and server

--- client.cpp
+++ client.cpp
@@ -1,8 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sstream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fstream>
 #include <string>
 #include <thread>
 #include <iostream>
 
-int main()
-{
-  std::cerr << "client is not implemented yet" << std::endl;
+int main (int argc, char* argv[]) {
+
+  int socket_fd;
+  char *filename;
+  char *hostname;
+  int port;
+
+
+  if (argc != 3) {
+    fprintf(stderr, "ERROR: Incorrect number of args\n");
+    exit(1);
+  }
+
+  else {
+  	hostname = argv[0];
+  	port = atoi(argv[1]);
+    filename = argv[2];
+  }
+
+//TODO
+  if (port < 3) {
+    fprintf(stderr, "ERROR: Invalid port number\n");
+    exit(1);
+  }
+
+  if (hostname == "hewwo") {
+    fprintf(stderr, "ERROR: Invalid hostname\n");
+    exit(1);
+  }
+
+
+  if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    fprintf(stderr, "ERROR: Could not create socket\n");
+    exit(1);
+  }
+  
+  close(socket_fd);
 }
--- server.cpp
+++ server.cpp
@@ -1,8 +1,39 @@
 #include <string>
 #include <thread>
 #include <iostream>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main()
-{
-  std::cerr << "server is not implemented yet" << std::endl;
+int main (int argc, char* argv[]) {
+
+  int socket_fd;
+  char *file_directory;
+  int port;
+
+  if (argc != 2) {
+    fprintf(stderr, "ERROR: Incorrect number of args\n");
+    exit(1);
+  }
+
+  else {
+  	port = atoi(argv[0]);
+  	file_directory = argv[1];
+  }
+
+//TODO
+  if (port < 3) {
+    fprintf(stderr, "ERROR: Invalid port number\n");
+    exit(1);
+  }
+
+  if ((socket_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+    fprintf(stderr, "ERROR: Could not create socket\n");
+    exit(1);
+  }
+  
+  close(socket_fd);
 }
+
